84569e7d5d2556b26006824bafadcaae0da4e0e7
mcicerani <mcicerani@gmail.com>
2024-09-12 18:03:01 +0200

    init

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero flex justify-center items-center">
+      <img src="logo.png" alt="hero" className="logo "/>
+    </div>
+  );
+}
+
+export default Hero;
