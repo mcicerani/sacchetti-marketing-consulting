5018aeed01c64d932166d34ce36a338feb403924
mcicerani <mcicerani@gmail.com>
2024-09-17 21:06:51 +0200

    mobile fix

--- src/components/Servizi.js
+++ src/components/Servizi.js
@@ -4,34 +4,35 @@ const Servizi = () => {
     return (
         <div className="sezione min-h-dvh lg:h-dvh">
             <h1 className="bg-logo text-white text-6xl lg:text-8xl">I Nostri Servizi</h1>
-            <div className="grid lg:grid-cols-3 lg:grid-rows-2 h-5/6 m-10 items-center justify-center gap-10">
-            <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
+            <div className="grid lg:grid-cols-3 lg:grid-rows-2 h-5/6 m-10 items-center justify-center gap-10 lg:p-5">
+            <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-10 lg:p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
                     <h2 className="text-3xl m-4">Social Media Manager</h2>
                     <p className="text-l">
                         Apertura e gestione periodica dei social network con pubblicazione di contenuti.
                     </p>
                 </div>
-                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
+                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-10 lg:p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
                     <h2 className="text-3xl m-4">Siti Web</h2>
                     <p className="text-l">
                         Realizzazione di siti web, blog, landing page con testi redatti in ottica SEO e SEM.<br></br>
                         Gestione e manutenzione periodica
                     </p>
                 </div>
-                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
-                    <h2 className="text-3xl m-4">Foto e Video</h2>
+                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-10 lg:p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
+                    <h2 className="text-3xl m-4">Foto, Video e Grafica</h2>
                     <p className="text-l">
-                        Organizzazione e realizzazione di shooting fotografici e video con Fotografi professionisti e Videomaker.
+                        Organizzazione e realizzazione di shooting fotografici e video con Fotografi professionisti e Videomaker.<br></br>
+                        Realizzazione grafica digitale e cartacea: volantini, brochure, locandine e gadget.
                     </p>
                 </div>
-                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
+                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-10 lg:p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
                     <h2 className="text-3xl m-4">Organizzazione Eventi</h2>
                     <p className="text-l">
                         Ci occupiamo di eventi a 360º: ricerca location, moderatori e relatori, sponsorizzazioni e
                         partnership, richiesta permessi burocratici, allestimento, comunicazione e ufficio stampa
                     </p>
                 </div>
-                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
+                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-10 lg:p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
                     <h2 className="text-3xl m-4">Consulenza Aziendale</h2>
                     <p className="text-l">
                         Valutazione dell'andamento e redazione del piano di marketing.<br></br>
@@ -40,7 +41,7 @@ const Servizi = () => {
                         Lancio di nuovi prodotti e progetti.<br></br>
                     </p>
                 </div>
-                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
+                <div className="servizio shadow-xl bg-gray-300/75 rounded-lg p-10 lg:p-5 text-center h-80 lg:h-5/6 flex flex-col items-center justify-center">
                     <h2 className="text-3xl m-4">Ufficio Stampa</h2>
                     <p className="text-l">
                         Redazione di comunicati stampa, invio a mailing list, monitoraggio online e offline della brand reputation.<br></br>
