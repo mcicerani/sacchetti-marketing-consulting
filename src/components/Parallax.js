023310e78253316ed2c6b3d9fc5a39180e466fa3
mcicerani <mcicerani@gmail.com>
2024-09-25 14:05:19 +0200

    parallax

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -5,37 +5,28 @@ import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
 const Parallax = () => {
   return (
     <ParallaxProvider>
-      <div className="container-body h-[50vh] w-full m-0 overflow-hidden">
+      <div className="container-body h-[50vh] w-full m-0 overflow-hidden opa">
         <ParallaxBanner
           layers={[
-            { image: "parallax.png", speed: -30 },  // Correzione del percorso
-            {
-              speed: -15,
-              children: (
-                <div className="">
-                  <h1 className="text">Hello World!</h1>
-                </div>
-              ),
-            },
             { image: "parallax.png", speed: -20 },  // Correzione del percorso
           ]}
           className="bg-container"
         >
           <div className="bg-container h-[50vh] relative flex flex-col w-full lg:w-auto lg:flex-row justify-evenly items-center">
-            <div className="h-40 p-4 border-[1px] w-full lg:w-auto border-white bg-white/50 flex justify-center items-center">
-                <h1 className="text-2xl lg:text-4xl text-black">Ascoltiamo</h1>
+            <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
+                <h1 className="text-2xl lg:text-4xl text-white">Ascoltiamo</h1>
             </div>
-            <div className="h-40 p-4 border-[1px] w-full lg:w-auto border-white bg-white/50 flex justify-center items-center">
-                <h1 className="text-2xl lg:text-4xl text-black">Pianifichiamo</h1>
+            <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
+                <h1 className="text-2xl lg:text-4xl text-white">Pianifichiamo</h1>
             </div>
-            <div className="h-40 p-4 border-[1px] w-full lg:w-auto border-white bg-white/50 flex justify-center items-center">
-                <h1 className="text-2xl lg:text-4xl text-black">Analizziamo</h1>
+            <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
+                <h1 className="text-2xl lg:text-4xl text-white">Analizziamo</h1>
             </div>
-            <div className="h-40 p-4 border-[1px] w-full lg:w-auto border-white bg-white/50 flex justify-center items-center">
-                <h1 className="text-2xl lg:text-4xl text-black">Progettiamo</h1>
+            <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
+                <h1 className="text-2xl lg:text-4xl text-white">Progettiamo</h1>
             </div>
-            <div className="h-40 p-4 border-[1px] w-full lg:w-auto border-white bg-white/50 flex justify-center items-center">
-                <h1 className="text-2xl lg:text-4xl text-black">Realizziamo</h1>
+            <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
+                <h1 className="text-2xl lg:text-4xl text-white">Realizziamo</h1>
             </div>
           </div>
         </ParallaxBanner>
