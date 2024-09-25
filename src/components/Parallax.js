348f816fc12b18f9c688647673ead0bc989c38ef
mcicerani <mcicerani@gmail.com>
2024-09-25 14:07:20 +0200

    order

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -17,10 +17,10 @@ const Parallax = () => {
                 <h1 className="text-2xl lg:text-4xl text-white">Ascoltiamo</h1>
             </div>
             <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
-                <h1 className="text-2xl lg:text-4xl text-white">Pianifichiamo</h1>
+                <h1 className="text-2xl lg:text-4xl text-white">Analizziamo</h1>
             </div>
             <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
-                <h1 className="text-2xl lg:text-4xl text-white">Analizziamo</h1>
+                <h1 className="text-2xl lg:text-4xl text-white">Pianifichiamo</h1>
             </div>
             <div className="h-40 p-4 border-[1px] w-full lg:w-72 border-white bg-logo/55 flex justify-center items-center lg:rounded-lg">
                 <h1 className="text-2xl lg:text-4xl text-white">Progettiamo</h1>
